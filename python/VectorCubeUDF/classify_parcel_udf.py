3023b83c80580fd20296d04995d63ed239e14cc9
Jeroen Dries <jeroen.dries@vito.be>
2023-06-19 16:32:03 +0200

    some improvements

--- python/VectorCubeUDF/classify_parcel_udf.py
+++ python/VectorCubeUDF/classify_parcel_udf.py
@@ -2,5 +2,5 @@ import pandas as pd
 from openeo.udf.debug import inspect
 
 def udf_apply_feature_dataframe(df: pd.DataFrame) -> float:
-    inspect(df)
+    inspect(message="logging from UDF!!",data=df)
     return 123.456
