c2ed4cbdd89668f06f5be6848ed982a86db5136c
Vincent <vincent@ptr-1w5qqg0uzr8itjt8twt.18120a2.ip6.access.telenet.be>
2024-01-12 15:29:29 +0100

    updated cloud and coverage scores to take larger neighboring areas into account

--- python/RankComposites/full_udf/udf_score.py
+++ python/RankComposites/full_udf/udf_score.py
@@ -18,6 +18,7 @@ def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
 
     clouds = np.logical_or(np.logical_and(cube_array < 11, cube_array >= 8), cube_array == 3).isel(bands=0)
 
+    # weights = [2, 0.8, 2]
     weights = [1, 0.8, 0.5]
 
     # Calculate the Day Of Year score
@@ -31,10 +32,14 @@ def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
     # Calculate the Distance To Cloud score
     # Erode
     # Source: https://github.com/dzanaga/satio-pc/blob/e5fc46c0c14bba77e01dca409cf431e7ef22c077/src/satio_pc/preprocessing/clouds.py#L127
-    e = footprints.disk(3)
+    er = footprints.disk(3)
+    di = footprints.disk(30)
     # Define a function to apply binary erosion
     def erode(image, selem):
-        return ~binary_erosion(image, selem)
+        return binary_erosion(image, selem)
+
+    def dilate(image, selem):
+        return ~binary_dilation(image, selem)
 
     # Use apply_ufunc to apply the erosion operation
     eroded = xr.apply_ufunc(
@@ -45,15 +50,27 @@ def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
         vectorize=True,  # vectorize the function over non-core dimensions
         dask="parallelized",  # enable dask parallelization
         output_dtypes=[np.int32],  # data type of the output
-        kwargs={'selem': e}  # additional keyword arguments to pass to erode
+        kwargs={'selem': er}  # additional keyword arguments to pass to erode
+    )
+
+    # Use apply_ufunc to apply the erosion operation
+    dilated = xr.apply_ufunc(
+        dilate,  # function to apply
+        eroded,  # input DataArray
+        input_core_dims=[['y', 'x']],  # dimensions over which to apply function
+        output_core_dims=[['y', 'x']],  # dimensions of the output
+        vectorize=True,  # vectorize the function over non-core dimensions
+        dask="parallelized",  # enable dask parallelization
+        output_dtypes=[np.int32],  # data type of the output
+        kwargs={'selem': di}  # additional keyword arguments to pass to erode
     )
 
     # Distance to cloud = dilation
     d_min = 0
-    d_req = 50
+    d_req = 100
     d = xr.apply_ufunc(
         distance_transform_cdt,
-        eroded,
+        dilated,
         input_core_dims=[['y', 'x']],
         output_core_dims=[['y', 'x']],
         vectorize=True,
@@ -71,6 +88,8 @@ def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
 
     # Final score is weighted average
     score = (weights[0] * score_clouds + weights[1] * score_doy + weights[2] * score_cov) / sum(weights)
+    # weights = [1, 0.5]
+    # score = (weights[0] * score_clouds + weights[1] * score_cov) / sum(weights)
     score = np.where(cube_array.values[:,0,:,:]==0, 0, score)
 
     score_da = xr.DataArray(
