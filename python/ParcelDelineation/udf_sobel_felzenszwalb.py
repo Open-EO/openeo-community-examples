e6c5065da436ef727dc846a1f7d09978390a6fc6
Manu Goudar <manu.goudar@vito.be>
2025-01-24 14:58:18 +0100

    Changed final segmentation udf to xarray from numpy.

--- python/ParcelDelineation/udf_sobel_felzenszwalb.py
+++ python/ParcelDelineation/udf_sobel_felzenszwalb.py
@@ -1,50 +1,22 @@
-import numpy as np
 from xarray import DataArray
-from skimage import segmentation
+from skimage import segmentation, graph
 from skimage.filters import sobel
-from skimage import graph
 
 
 def apply_datacube(cube: DataArray, context: dict) -> DataArray:
-    # get the underlying numpy array without the bands and t dimension
-    np_array = cube.squeeze("t", drop=True)\
-        .squeeze("bands", drop=True)\
-        .values
-
+    # get the underlying array without the bands and t dimension
+    _data = cube.squeeze("t", drop=True).squeeze("bands", drop=True).values
     # compute edges
-    edges = sobel(np_array)
-
+    edges = sobel(_data)
     # Perform felzenszwalb segmentation
-    segment = np.array(
-        segmentation.felzenszwalb(np_array, scale=120, sigma=0.0, min_size=30, channel_axis=None)
-    ).astype(np.int32)
-
+    segment = segmentation.felzenszwalb(_data, scale=120, sigma=0.0, min_size=30, channel_axis=None)
     # Perform the rag boundary analysis and merge the segments
     bgraph = graph.rag_boundary(segment, edges)
     # merging segments
     mergedsegment = graph.cut_threshold(segment, bgraph, 0.15, in_place=False)
+    # create a data cube and perform masking operations
+    output_arr = DataArray(mergedsegment.reshape(cube.shape), dims=cube.dims, coords=cube.coords)
+    output_arr = output_arr.where(cube >= 0.3)   # Mask the output pixels based on the cube values <0.3
+    output_arr = output_arr.where(output_arr >= 0)  # Mask all values less than or equal to zero
+    return output_arr
 
-    # create random numbers for the segments
-    unique_classes = np.unique(mergedsegment)
-    random_numbers = np.random.randint(0, 1000000, size=len(np.unique(mergedsegment)))
-
-    counter = 0
-    for unique_class in unique_classes:
-        if unique_class == 0:
-            continue
-        mergedsegment[mergedsegment == unique_class] = random_numbers[counter]
-        counter += 1
-
-    mergedsegment = mergedsegment.astype(float)
-    mergedsegment[np_array < 0.3] = np.nan
-    mergedsegment[mergedsegment < 0] = 0
-
-    outarr = DataArray(
-        mergedsegment.reshape(cube.shape),
-        dims=cube.dims,
-        coords=cube.coords,
-    )
-    outarr = outarr.astype(np.float64)
-    outarr = outarr.where(outarr != 0, np.nan)
-
-    return outarr
