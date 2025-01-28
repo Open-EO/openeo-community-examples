b9527387ddc140641579e24a9557cc36dae28514
Manu Goudar <manu.goudar@vito.be>
2025-01-28 17:03:55 +0100

    Parameterized code: changed hard coded parameters.

--- python/ParcelDelineation/udf_segmentation.py
+++ python/ParcelDelineation/udf_segmentation.py
@@ -49,11 +49,12 @@ def process_window_onnx(ndvi_stack: xr.DataArray, patch_size=128):
         Size of the sample
 
     """
-    # we'll do 12 predictions: use 3 networks, and for each random take 3 NDVI bands and repeat 4 times
+    # we'll do 12 predictions: use 3 networks, and for each random take 3 NDVI images and repeat 4 times
     ort_sessions = load_ort_sessions(model_names)    # get models
     predictions_per_model = 4
+    no_rand_images = 3          # Number of random images that are needed for input
     no_images = ndvi_stack.t.shape[0]
-
+    
     # Range of index of images
     _range = range(no_images)
     # List of all predictions
@@ -63,9 +64,9 @@ def process_window_onnx(ndvi_stack: xr.DataArray, patch_size=128):
         for i in range(predictions_per_model):
             # initialize a predicter array
             random.seed(i)   # without seed we will have random number leading to non-reproducable results.
-            _idx = random.choices(_range, k=3)
+            _idx = random.choices(_range, k=no_rand_images) # Random selection of 3 images for input
             # re-shape the input data for ML input 
-            input_data = ndvi_stack.isel(t=_idx).data.reshape(1, patch_size * patch_size, 3)
+            input_data = ndvi_stack.isel(t=_idx).data.reshape(1, patch_size * patch_size, no_rand_images)
             ort_inputs = {ort_session.get_inputs()[0].name: input_data}
 
             # Run ML to predict
@@ -86,7 +87,7 @@ def process_window_onnx(ndvi_stack: xr.DataArray, patch_size=128):
     return all_predictions.median(dim="predict")
 
 
-def preprocess_datacube(cubearray: xr.DataArray):
+def preprocess_datacube(cubearray: xr.DataArray, min_images: int):
     # check if bands is in the dims and select the first index
     if "bands" in cubearray.dims:
         nvdi_stack = cubearray.isel(bands=0)
@@ -106,21 +107,24 @@ def preprocess_datacube(cubearray: xr.DataArray):
 
     # Select all clear images (without ANY missing values)
     # or select the 3 best ones (contain nans)
-    if (sum_invalid.data == 0).sum() > 3:
+    if (sum_invalid.data == 0).sum() > min_images:
         good_data = nvdi_stack_data.sel(t = sum_invalid[sum_invalid.data == 0].t)
     else:
-        good_data = nvdi_stack_data.sel(t = sum_invalid.sortby(sum_invalid).t[:3])
+        good_data = nvdi_stack_data.sel(t = sum_invalid.sortby(sum_invalid).t[:min_images])
     return good_data.transpose("x", "y", "t")
 
 
 def apply_datacube(cube: xr.DataArray, context: Dict) -> xr.DataArray:
+    # select atleast best 3 temporal images of ndvi for ML
+    min_images = 3
+    
     # preprocess the datacube
-    ndvi_stack = preprocess_datacube(cube)
+    ndvi_stack = preprocess_datacube(cube, min_images)
 
     # check number of images after preprocessing
-    # if the stack doesn't have at least 3 bands, we cannot process this window
-    nr_valid_bands = ndvi_stack.t.shape[0]
-    if nr_valid_bands < 3:
+    # if the stack doesn't have at least 3 temporal images, we cannot process this window
+    nr_valid_images = ndvi_stack.t.shape[0]
+    if nr_valid_images < min_images:
         inspect(message="Not enough input data for this window -> skipping!")
         return None
 
