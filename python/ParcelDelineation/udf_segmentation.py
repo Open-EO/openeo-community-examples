742f36061831b02e08b557cd3d06352b5c95fcf8
Manu <manu.goudar@vito.be>
2025-02-26 16:10:48 +0100

    Refinement of parcel delineation community notebook.
    * Convert to xarray processing from numpy.
    * Make results reproducable by adding random.seed
    * Working with (_old) and without numpy.
    * Removed numpy and old code in ML segmentation udf.
    * Changed final segmentation udf to xarray from numpy.
    * Parameterized code: changed hard coded parameters.
    * Added proper inlay hint: Dict from typing.
    * Removed the time filtering. Added strict cloud filtering.
    * Changed the sys path insert location.
    * Changed lru_cache value. And removed unused variables.
    * Check for invalid data chunks.
    * Added cloud mask.
    * Added comments and function doc-strings.
    
    All PR comments resolved.

--- python/ParcelDelineation/udf_segmentation.py
+++ python/ParcelDelineation/udf_segmentation.py
@@ -2,16 +2,14 @@ import functools
 import gc
 import sys
 from typing import Dict
-
-import numpy as np
+import random
 import xarray as xr
 from openeo.udf import inspect
-from xarray.ufuncs import isnan as ufuncs_isnan
-from xarray import DataArray
 
 # Add the onnx dependencies to the path
-sys.path.insert(0, "onnx_deps")
+sys.path.insert(1, "onnx_deps")
 import onnxruntime as ort
+
 
 model_names = frozenset([
     "BelgiumCropMap_unet_3BandsGenerator_Network1.onnx",
@@ -19,7 +17,8 @@ model_names = frozenset([
     "BelgiumCropMap_unet_3BandsGenerator_Network3.onnx",
 ])
 
-@functools.lru_cache(maxsize=25)
+
+@functools.lru_cache(maxsize=1)
 def load_ort_sessions(names):
     """
     Load the models and make the prediction functions.
@@ -34,117 +33,137 @@ def load_ort_sessions(names):
         for model_name in names
     ]
 
-def process_window_onnx(ndvi_stack, patch_size=128):
-    ## check whether you actually supplied 3 images or more
-    nr_valid_bands = ndvi_stack.shape[2]
 
-    ## if the stack doesn't have at least 3 bands, we cannot process this window
-    if nr_valid_bands < 3:
-        inspect(message="Not enough input data for this window -> skipping!")
-        # gc.collect()
-        return None
+def process_window_onnx(ndvi_stack: xr.DataArray, patch_size=128) -> xr.DataArray:
+    """Compute prediction.
+
+    Compute predictions using ML models. ML models takes three inputs images and predicts
+    one image. Four predictions are made per model using three random images. Three images
+    are considered to save computational time. Final result is median of these predictions.
+
+    Parameters
+    ----------
+    ndvi_stack : DataArray
+        ndvi data
+    patch_size : Int
+        Size of the sample
+
+    Returns
+    -------
+    xr.DataArray
+        Machine learning prediction.
+    """
+    # we'll do 12 predictions: use 3 networks, and for each random take 3 NDVI images and repeat 4 times
+    ort_sessions = load_ort_sessions(model_names)    # get models
+
+    predictions_per_model = 4
+    no_rand_images = 3          # Number of random images that are needed for input
+    no_images = ndvi_stack.t.shape[0]
     
-    ## we'll do 12 predictions: use 3 networks, and for each randomly take 3 NDVI bands and repeat 4 times
-    ort_sessions = load_ort_sessions(model_names)
-    number_of_models = len(ort_sessions)
-    number_per_model = 4
+    # Range of index of images
+    _range = range(no_images)
+    # List of all predictions
+    prediction = []
+    for ort_session in ort_sessions:
+        # make 4 predictions per model
+        for i in range(predictions_per_model):
+            # initialize a predicter array
+            random.seed(i)   # without seed we will have random number leading to non-reproducible results.
+            _idx = random.choices(_range, k=no_rand_images) # Random selection of 3 images for input
+            # re-shape the input data for ML input 
+            input_data = ndvi_stack.isel(t=_idx).data.reshape(1, patch_size * patch_size, no_rand_images)
+            ort_inputs = {ort_session.get_inputs()[0].name: input_data}
 
-    prediction = np.zeros((patch_size, patch_size, number_of_models * number_per_model))
-
-    for model_counter in range(number_of_models):
-        ## load the current model
-        ort_session = ort_sessions[model_counter]
-
-        ## make 4 predictions per model
-        for i in range(number_per_model):
-            ## define the input data
-            input_data = ndvi_stack[
-                                :,
-                                :,
-                                np.random.choice(
-                                    np.arange(nr_valid_bands), size=3, replace=False
-                                ),
-                            ].reshape(1, patch_size * patch_size, 3)
-            ort_inputs = {
-                ort_session.get_inputs()[0].name: input_data
-            }
-
-            ## make the prediction
+            # Run ML to predict
             ort_outputs = ort_session.run(None, ort_inputs)
-            prediction[:, :, i + number_per_model*model_counter] =  np.squeeze(
-                ort_outputs[0]\
-                    .reshape((patch_size, patch_size))
-            )
+            # reshape ort_outputs and append it to prediction list
+            prediction.append(ort_outputs[0].reshape((patch_size, patch_size)))
 
-    ## free up some memory to avoid memory errors
+    # free up some memory to avoid memory errors
     gc.collect()
 
-    ## final prediction is the median of all predictions per pixel
-    final_prediction = np.median(prediction, axis=2)
-    return final_prediction
+    # Create a DataArray of all predictions
+    all_predictions = xr.DataArray(prediction, dims=["predict", "x", "y"],
+                                   coords={"predict": range(len(prediction)),
+                                           "x": ndvi_stack.coords["x"],
+                                           "y": ndvi_stack.coords["y"]}
+                                   )
+    # final prediction is the median of all predictions per pixel
+    return all_predictions.median(dim="predict")
 
-def preprocess_datacube(cubearray):
-    ## xarray nan to np nan (we will pass a numpy array)
-    cubearray = cubearray.where(~ufuncs_isnan(cubearray), np.nan)
 
-    ## Transpose to format accepted by model and get the values
-    cubearray = cubearray.transpose("x", "bands", "y", "t")
-    cubearray = cubearray[:, 0, :, :]
-    ndvi_stack = cubearray.transpose("x", "y", "t").values
+def preprocess_datacube(cubearray: xr.DataArray, min_images: int) -> tuple[bool, xr.DataArray]:
+    """Preprocess data for machine learning.
 
-    ## Clamp out of range NDVI values
-    ndvi_stack = np.where(ndvi_stack < 0.92, ndvi_stack, 0.92)
-    ndvi_stack = np.where(ndvi_stack >-0.08, ndvi_stack, np.nan)
-    ndvi_stack = (ndvi_stack + 0.08)
+    Preprocess data by clamping NVDI values and first check if the
+    data is valid for machine learning and then check if there is good
+    data to perform machine learning.
 
-    ## Create a mask where all valid values are 1 and all nans are 0
-    ## This will be used for selecting the best images
-    mask_stack = np.ones_like(ndvi_stack, dtype=np.uint8)
-    mask_stack = np.where(np.isnan(ndvi_stack), 0, 1)
+    Parameters
+    ----------
+    cubearray : xr.DataArray
+        Input datacube
+    min_images : int
+        Minimum number of samples to consider for machine learning.
 
-    ## Fill the NaN values with 0
-    ndvi_stack[mask_stack == 0] = 0
-
-    ## Count the amount of invalid data per acquisition and sort accordingly
-    sum_invalid = np.sum(mask_stack == 0, axis=(0, 1))
-
-    ## If we have enough clear images (without ANY missing values), we're good to go, 
-    ## and we will use all of them (could be more than 3!)
-    if len(np.where(sum_invalid == 0)[0]) > 3:
-        ndvi_stack = ndvi_stack[:, :, np.where(sum_invalid == 0)[0]]
-
-    ## else we need to add some images that do contain some nan's; 
-    ## in this case we will select just the 3 best ones
+    Returns
+    -------
+    bool
+        True refers to data is invalid for machine learning.
+    xr.DataArray
+        If above bool is False, return data for machine learning else returns a
+        sample containing nan (similar to machine learning output).
+    """
+    # Preprocessing data
+    # check if bands is in the dims and select the first index
+    if "bands" in cubearray.dims:
+        nvdi_stack = cubearray.isel(bands=0)
     else:
-        # inspect(f"Found {len(np.where(sum_invalid == 0)[0])} clear acquisitions -> appending some bad images as well!")
-        idxsorted = np.argsort(sum_invalid)
-        ndvi_stack = ndvi_stack[:, :, idxsorted[:3]]
+        nvdi_stack = cubearray
+    # Clamp out of range NDVI values
+    nvdi_stack = nvdi_stack.where(lambda nvdi_stack: nvdi_stack < 0.92, 0.92)
+    nvdi_stack = nvdi_stack.where(lambda nvdi_stack: nvdi_stack > -0.08)
+    nvdi_stack += 0.08
+    # Count the amount of invalid pixels in each time sample. 
+    sum_invalid = nvdi_stack.isnull().sum(dim=['x', 'y'])
+    # Check % of invalid pixels in each time sample by using mean
+    sum_invalid_mean = nvdi_stack.isnull().mean(dim=['x', 'y'])
+    # Fill the invalid pixels with value 0
+    nvdi_stack_data = nvdi_stack.fillna(0)
 
-    ## return the stack
-    return ndvi_stack
+    # Check if data is valid for machine learning. If invalid, return True and
+    # an DataArray of nan values (similar to the machine learning output)
+    if (sum_invalid_mean.data < 1).sum() <= min_images:   # number of invalid time sample less then min images
+        inspect(message="Input data is invalid for this window -> skipping!")
+        # create a nan dataset and return
+        nan_data = xr.zeros_like(nvdi_stack.sel(t = sum_invalid_mean.t[0], drop=True))
+        nan_data = nan_data.where(lambda nan_data: nan_data > 1)
+        return True, nan_data
 
-def apply_datacube(cube: DataArray, context: Dict) -> DataArray:
-    ## preprocess the datacube
-    ndvi_stack = preprocess_datacube(cube)
+    # Data selection: valid data for machine learning
+    # select time samples where there are no invalid pixels
+    if (sum_invalid.data == 0).sum() >= min_images:
+        good_data = nvdi_stack_data.sel(t = sum_invalid[sum_invalid.data == 0].t)
+    else:      # select the 4 best time samples with least amount of invalid pixels.
+        good_data = nvdi_stack_data.sel(t = sum_invalid.sortby(sum_invalid).t[:min_images])
+    return False, good_data.transpose("x", "y", "t")
 
-    ## process the window
+
+def apply_datacube(cube: xr.DataArray, context: Dict) -> xr.DataArray:
+    # select atleast best 4 temporal images of ndvi for ML
+    min_images = 4
+    
+    # preprocess the datacube
+    invalid_data, ndvi_stack = preprocess_datacube(cube, min_images)
+
+    # If data is invalid, there is no need to run prediction algorithm so
+    # return prediction as nan DataArray and reintroduce time and bands dimensions 
+    if invalid_data:
+        return ndvi_stack.expand_dims(dim={"t": [(cube.t.dt.year.values[0])], "bands": ["prediction"]})
+    
+    # Machine learning prediction: process the window
     result = process_window_onnx(ndvi_stack)
-
-    ## transform your numpy array predictions into an xarray
-    result = result.astype(np.float64)
-    result_xarray = xr.DataArray(
-        result,
-        dims=["x", "y"],
-        coords={"x": cube.coords["x"], "y": cube.coords["y"]},
-    )
-
-    ## Reintroduce time and bands dimensions
-    result_xarray = result_xarray.expand_dims(
-        dim={
-            "t": [np.datetime64(str(cube.t.dt.year.values[0]) + "-01-01")], 
-            "bands": ["prediction"],
-        },
-    )
-
-    ## Return the resulting xarray
+    # Reintroduce time and bands dimensions
+    result_xarray = result.expand_dims(dim={"t": [(cube.t.dt.year.values[0])], "bands": ["prediction"]})
+    # Return the resulting xarray
     return result_xarray
